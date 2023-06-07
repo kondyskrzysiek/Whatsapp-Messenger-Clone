b8c6a11ebe58497098243bc33fdc927ced43fb2e
KrzysztofKondys <krzysiekkondys@gmail.com>
2023-06-07 12:14:03 +0200

    create new branch

---
+++ lib/feature/auth/widgets/custom_text_fields.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:whatsapp_messenger/common/extension/custom_theme_extension.dart';
+import 'package:whatsapp_messenger/common/utils/coloors.dart';
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({
+    super.key,
+    this.controller,
+    this.hintText,
+    this.readOnly,
+    this.textAlign,
+    this.keyboardType,
+    this.prefixText,
+    this.onTap,
+    this.suffixIcon,
+    this.onChanged,
+    this.fontSize,
+    this.autoFocus,
+  });
+
+  final TextEditingController? controller;
+  final String? hintText;
+  final bool? readOnly;
+  final TextAlign? textAlign;
+  final TextInputType? keyboardType;
+  final String? prefixText;
+  final VoidCallback? onTap;
+  final Widget? suffixIcon;
+  final Function(String)? onChanged;
+  final double? fontSize;
+  final bool? autoFocus;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      onTap: onTap,
+      controller: controller,
+      readOnly: readOnly ?? false,
+      textAlign: textAlign ?? TextAlign.center,
+      keyboardType: readOnly == null ? keyboardType : null,
+      onChanged: onChanged,
+      style: TextStyle(fontSize: fontSize),
+      autofocus: autoFocus ?? false,
+      decoration: InputDecoration(
+        isDense: true,
+        prefixText: prefixText,
+        hintText: hintText,
+        suffix: suffixIcon,
+        hintStyle: TextStyle(color: context.theme.greyColor),
+        enabledBorder: const UnderlineInputBorder(
+          borderSide: BorderSide(color: Coloors.greenDark),
+        ),
+        focusedBorder: const UnderlineInputBorder(
+          borderSide: BorderSide(color: Coloors.greenDark, width: 2),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp_messenger/common/theme/dark_theme.dart';
+import 'package:whatsapp_messenger/common/theme/light_theme.dart';
+import 'package:whatsapp_messenger/feature/welcome/pages/welcome_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'common/routes/routes.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'WhatsApp Me',
+      theme: lightTheme(),
+      darkTheme: darkTheme(),
+      themeMode: ThemeMode.system,
+      home: const WelcomePage(),
+      onGenerateRoute: Routes.onGenerateRoute,
+    );
+  }
+}
